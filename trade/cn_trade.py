409d1517a0edf1154ffa5f59da44212da476a5ae
frankyzhou <lijunzhou-fudan@qq.com>
2016-09-06 10:16:11 +0800

    fix amount

--- trade/cn_trade.py
+++ trade/cn_trade.py
@@ -47,7 +47,7 @@ class CNTrade(object):
                 else:
                     result["trade"] = "买入不足100股 "+code+" @ " + str(price) + " 共 " + str(amount)
         elif dif < 0:
-		    amount = enable_amount if dif == -2 else min(enable_amount, amount)
+            amount = enable_amount if dif == -2 else min(enable_amount, amount)
             if amount >= 100:
 				result = str_to_dict(self.client.exec_order("sell " + code + " " + str(price) + " " + str(amount)))
 				result["trade"] = "卖出 "+code+" @ " + str(price) + " 共 " + str(amount)
