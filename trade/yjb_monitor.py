cb17fc7aee881c298e3351028cfbb95c8b367cf8
frankyzhou <lijunzhou-fudan@qq.com>
2016-09-29 17:28:35 +0800

    fix yjb position is string problem

--- trade/yjb_monitor.py
+++ trade/yjb_monitor.py
@@ -3,7 +3,9 @@ import sys
 # c = client(host="10.104.236.87")
 c = client(host="127.0.0.1")
 while 1:
-    print "Enter data to transmit: stop, get_position, buy, sell"
-    data = sys.stdin.readline().strip()
-    print c.exec_order(data)
-    print "\n"
+    try:
+        print "Enter data to transmit: stop, get_position, buy, sell"
+        data = sys.stdin.readline().strip()
+        print c.exec_order(data)
+    except Exception, e:
+        print e
