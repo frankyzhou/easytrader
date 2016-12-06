a90b7da5068acac51d27f2b57d0b3637fe6cea7c
frankyzhou <lijunzhou-fudan@qq.com>
2016-12-06 13:59:17 +0800

    ths

--- test/testTHS.py
+++ test/testTHS.py
@@ -4,9 +4,12 @@ import time
 client = client(host="10.104.236.87")
 all = ts.get_today_all()
 codes = all["code"].values
+black_list = ["300126"] 
 for code in codes:
-    order = "buy " + code + " 0.01 100"
-    result = client.exec_order(order)
-    if result != "OK":
-        print order
-    time.sleep(3)
+    if code not in black_list:
+        order = "buy " + code + " 0.01 100"
+        result = client.exec_order(order)
+        if result != "OK":
+            print order, result
+            break
+        time.sleep(3)
