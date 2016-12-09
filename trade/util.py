04d7c206f3fdb5a57ec31be03716b0a23b0d7d20
frankyzhou <lijunzhou-fudan@qq.com>
2016-12-09 16:27:39 +0800

    timeout

--- trade/util.py
+++ trade/util.py
@@ -187,10 +187,14 @@ def get_client(host='127.0.0.1', textport=51500, timeout=15):
 
 def get_code_name(all_data, code, name=None):
     n = all_data[["code", "name"]]
-    if name:
-        return n[n.name == name].values[0][0]
-    else:
-        return n[n.code == code].values[0][1]
+    try:
+        if name:
+            return n[n.name == name].values[0][0]
+        else:
+            return n[n.code == code].values[0][1]
+    except Exception, e:
+        print e
+        return code if not name else name
 
 
 class client:
